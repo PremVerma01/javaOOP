94483b6f70584497ad0292161ba66b80355528c5
Premnarayan Verma <premvrm01@gmail.com>
2024-09-12 14:53:45 +0530

    Add files via upload

---
+++ DogElc.java
@@ -0,0 +1,17 @@
+package com.java.oops;
+
+public class DogElc {
+
+	public static void main(String[] args)
+	{
+	  DogBlc sandy=new DogBlc();
+	  sandy.age=4;
+	  sandy.name="sandy bau";
+	  sandy.height=3.5;
+	  
+	  sandy.getDogInformation();
+	  sandy.bark();
+
+	}
+
+}
