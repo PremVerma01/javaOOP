225c003311cffb1ec18b80e68f5bd764a87de0b4
Premnarayan Verma <premvrm01@gmail.com>
2024-09-12 14:54:44 +0530

    Add files via upload

---
+++ StudentElc.java
@@ -0,0 +1,19 @@
+package com.java.oops;
+
+public class StudentElc
+{
+
+	public static void main(String[] args) 
+	{
+		StudentBlc prem=new StudentBlc();
+		prem.rollnumber=112;
+		prem.name="premVerma";
+		prem.height=6.1;
+		prem.fee=1001;
+		
+		prem.properties();
+		prem.functionalities();
+		
+	}
+
+}
