7f47af0f86e45f534098ec1734c235bb55caf835
Premnarayan Verma <premvrm01@gmail.com>
2024-09-12 14:54:26 +0530

    Add files via upload

---
+++ StudentBlc.java
@@ -0,0 +1,25 @@
+package com.java.oops;
+
+public class StudentBlc
+{
+   int rollnumber;
+   double fee;
+   String name;
+   double height;
+ 
+   
+   
+     public void properties()
+     {
+    	 System.out.println("student name is "+name);
+    	 System.out.println("student name is "+rollnumber);
+    	 System.out.println("student fee is "+fee);
+    	 
+    	 System.out.println("student height is "+height);
+     }
+     public void functionalities()
+     {
+    	 System.out.println(name+" can run very faster than me his is this "+rollnumber+" pending fee is "+fee);
+     }
+     
+}
