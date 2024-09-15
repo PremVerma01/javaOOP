654e6b747148bf93b1980ca9c71539636f9aee62
Premnarayan Verma <premvrm01@gmail.com>
2024-09-15 09:16:44 +0530

    Add files via upload

---
+++ Customer2Blc.java
@@ -0,0 +1,23 @@
+package com.java.oops;
+
+public class Customer2Blc
+{
+	int number;
+	String name;
+	int id;
+	String shift;
+ 
+	   public void CustomerData()
+	   {
+		   System.out.println("the number is "+number );
+		   System.out.println("and name is "+name);
+		   System.out.println("your id is "+id);
+		  //prem System.out.println("enter shift "+shift);
+	   }
+	   
+	   public void CustomerFunc()
+	   {
+		   System.out.println(name+" can do this work better "+id+" is his id ");
+	   }
+
+}
