2e8646a24298caacf8f500b1d1f5424f522d869d
Premnarayan Verma <premvrm01@gmail.com>
2024-09-15 09:17:14 +0530

    Add files via upload

---
+++ CustomerBlc.java
@@ -0,0 +1,19 @@
+package com.java.oops;
+
+public class CustomerBlc
+{
+	
+	int number;
+	String name;
+	int id;
+	int bill;
+	
+   public void CustBill()
+   {
+	      System.out.println("yur bill is"+bill);
+   }
+   public void CustDetails()
+   {
+	      System.out.println("your name is "+name+"your id is "+id+" and your number is "+number);
+   }
+}
